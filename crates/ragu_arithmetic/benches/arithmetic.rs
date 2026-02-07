ba2adbf39ae614c9edca40683030818fff5051ad
Tal Derei <talderei99@gmail.com>
2026-02-07 12:51:17 -0800

    ci: fix ci compilation

--- crates/ragu_arithmetic/benches/arithmetic.rs
+++ crates/ragu_arithmetic/benches/arithmetic.rs
@@ -2,7 +2,7 @@ mod setup;
 
 use gungraun::{library_benchmark, library_benchmark_group, main};
 use pasta_curves::{EpAffine, Fp, Fq};
-use ragu_ragu_arithmetic::{Domain, dot, eval, factor, geosum, mul, poly_with_roots};
+use ragu_arithmetic::{Domain, dot, eval, factor, geosum, mul, poly_with_roots};
 use setup::{f, setup_domain_ell, setup_domain_fft, setup_rng, setup_with_rng, vec_affine, vec_f};
 use std::hint::black_box;
 
